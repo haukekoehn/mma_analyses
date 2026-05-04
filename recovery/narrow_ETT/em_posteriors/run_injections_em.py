4a75f2d020f13c012c97b61479e69744f79b99c6
haukekoehn <hauke.koehn@uni-potsdam.de>
2026-05-04 11:33:42 +0200

    recovery: narrow_ETT EM complete

--- recovery/narrow_ETT/em_posteriors/run_injections_em.py
+++ recovery/narrow_ETT/em_posteriors/run_injections_em.py
@@ -150,8 +150,8 @@ def analyze_event(j, param_dict, rng_key):
 def main():
 
     events = pd.read_csv("../events.dat", sep=" ")
-    rng_key = jax.random.PRNGKey(888)
-    for j in range(0, events.shape[0]):
+    rng_key = jax.random.PRNGKey(18930)
+    for j in range(events.shape[0]):
         rng_key, sub_key = jax.random.split(rng_key)
         try:
             analyze_event(j, events.iloc[j].to_dict(), sub_key)
