a045f734b6dfd7e803dced6a59c8d5c2ac877420
haukekoehn <hauke.koehn@uni-potsdam.de>
2026-05-21 11:39:13 +0200

    wide ETL GW complete

--- eos_inference/wide_ETL/gw_posteriors/submit_injections_gw.sh
+++ eos_inference/wide_ETL/gw_posteriors/submit_injections_gw.sh
@@ -26,7 +26,7 @@ export OMP_NUM_THREADS=1
 
 mkdir -p source_${src}
 
-srun python run_injections_gw.py --source ${src} --outdir source_${src} --random-seed 148596
+srun python run_injections_gw.py --source ${src} --outdir source_${src} --random-seed 161
 EOF
 
 done
